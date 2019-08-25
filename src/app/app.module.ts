747ea05e232a798d1a3c0fcb1dfb373741e846d0
Thiago Corbalan - PC CASA <contato.twebdesigner@gmail.com>
2019-08-24 22:21:27 -0300

    HTML markup of content and component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
-import { MatInputModule } from '@angular/material';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule, MatOptionModule, MatFormFieldModule, MatSelectModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { AsideComponent } from './layout/aside/aside.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
       HeaderComponent,
-      FooterComponent
+      FooterComponent,
+      AsideComponent,
+      PaymentComponent,
+      CreditCardComponent
    ],
    imports: [
       BrowserModule,
       BrowserAnimationsModule,
       MatFormFieldModule,
-      MatInputModule
+      MatInputModule,
+      MatOptionModule,
+      MatSelectModule
    ],
    providers: [],
    bootstrap: [
